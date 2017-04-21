8b52b602478b5261164417e7b4b21bcd1f7920c9
duweimin <844652405@qq.com>
2017-04-21 20:47:04 +0800

    '上传第一个页面'

--- components/video/video.js
+++ components/video/video.js
@@ -8,9 +8,23 @@ angular.module('videoModule',[])
 			css:'components/video/video.css'
 		});
 })
-.controller('videoControl',['$scope','$http',function($scope,$http){
-	console.log('ss');
+.service('swiper',['$timeout',function($timeout){
+	$timeout(function(){
+		var mySwiper = new Swiper ('.swiper-container', {
+		    direction: 'horizontal',
+		    loop: true,
+		    autoplay:500,
+		    pagination: '.swiper-pagination',  
+		 }) 
+	},50);
+}])
+.controller('videoControl',['$scope','$http','swiper',function($scope,$http){
 		$http.get('components/video/json/轮播.json').success(function(res){
-			$scope.arr=res.msg.reclist_236;
+			$scope.arr=res.msg.reclist_236;	
 		});
+		$http.get('components/video/json/赛事.json').success(function(res){
+			$scope.arr1=res.msg.result;	
+		});
+
 }]);
+    
